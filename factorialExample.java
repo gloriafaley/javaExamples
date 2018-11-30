039b43f033f19bcbf4b28b885168c19561519e01
Gloria Faley <45109944+gloriafaley@users.noreply.github.com>
2018-11-30 14:02:35 -0500

    Create factorialExample.java

---
+++ factorialExample.java
@@ -0,0 +1,23 @@
+import java.util.*; 
+import java.io.*;
+
+class Main {  
+  public static int FirstFactorial(int num) { 
+  
+    if (num == 0)    
+            return 1;    
+      else  {
+            num = num * FirstFactorial(num-1);
+            return num;
+       }  
+           
+    
+  } 
+  
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(FirstFactorial(s.nextLine())); 
+  }   
+  
+}
